766c89f3111e21d47eaa9779db8ea5c23cffa937
Marcus Reis <mvsantosreis@gmail.com>
2024-03-21 20:36:27 +0000

    Loading form (part 2)

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -20,44 +20,51 @@ function submit() {
 </script>
 <template>
   <v-container fluid>
-    <v-card class="pa-4 mx-auto" width="300">
-      <v-card-title class="text-center">Login</v-card-title>
-      <v-card-item>
-        <v-form @submit.prevent="submit">
-          <v-text-field
-            variant="solo"
-            prepend-inner-icon="mdi-email"
-            v-model="form.email"
-            label="Email"
-          ></v-text-field>
+    <v-row justify="center">
+      <v-col md="4">
+        <v-overlay :model-value="isLoading">
+          <v-progress-circular v-if="isLoading" indeterminate color="white">
+          </v-progress-circular>
+        </v-overlay>
 
-          <v-text-field
-            variant="solo"
-            prepend-inner-icon="mdi-key"
-            v-model="form.password"
-            label="Password"
-          ></v-text-field>
+        <v-card class="pa-4 mx-auto" width="300">
+          <v-card-title class="text-center">Login</v-card-title>
+          <v-card-item>
+            <v-form @submit.prevent="submit">
+              <v-text-field
+                variant="solo"
+                prepend-inner-icon="mdi-email"
+                v-model="form.email"
+                label="Email"
+              ></v-text-field>
 
-          <v-checkbox
-            v-model="form.remember"
-            label="Remember me"
-            color="red"
-            hide-details
-          ></v-checkbox>
+              <v-text-field
+                variant="solo"
+                prepend-inner-icon="mdi-key"
+                v-model="form.password"
+                label="Password"
+              ></v-text-field>
 
-          <v-btn color="red-darken-1" class="mt-2" type="submit" block>
-            <v-progress-circular v-if="isLoading" indeterminate color="white">
-            </v-progress-circular>
-            <span v-else>Submit</span>
-          </v-btn>
-        </v-form>
-      </v-card-item>
+              <v-checkbox
+                v-model="form.remember"
+                label="Remember me"
+                color="red"
+                hide-details
+              ></v-checkbox>
 
-      <v-card-action>
-        <div class="mx-4">
-          <v-btn block to="/register"> Register </v-btn>
-        </div>
-      </v-card-action>
-    </v-card>
+              <v-btn color="red-darken-1" class="mt-2" type="submit" block>
+                <span>Submit</span>
+              </v-btn>
+            </v-form>
+          </v-card-item>
+
+          <v-card-action>
+            <div class="mx-4">
+              <v-btn block to="/register"> Register </v-btn>
+            </div>
+          </v-card-action>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
